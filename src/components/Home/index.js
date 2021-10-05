06f2f32e8c4f0b3dca4380121fdc820fa0346cc4
insansantana <insan.santana@gmail.com>
2021-10-05 13:52:10 +0700

    Menambahkan file resume.pdf

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import  Typewriter  from 'typewriter-effect'
-import { FaSmileBeam } from 'react-icons/fa'
+import { FaSmileBeam,FaFileDownload } from 'react-icons/fa'
 import { motion } from 'framer-motion'
+import { IconContext } from 'react-icons'
+
+
 const Home = () => {
     return (
         <div className="home min-w-full flex flex-grow flex-col justify-center items-center md:flex-row md:justify-between">
@@ -14,8 +17,14 @@ const Home = () => {
                     />
                 </h1>
                 <br/>
-                
-                <button><Link className="rounded-full py-3 px-6 bg-blue-400 text-white text-sm md:text-lg" to="/about">More Info</Link></button>
+                <div className="flex items-center">
+                    <button className="rounded-md py-3 px-6 mr-4 bg-blue-400 text-white text-sm md:text-md">
+                        <Link  to="/about">More Info</Link>
+                    </button>
+                        <a href="./pdf/cv.pdf" className="rounded-md py-3 px-6 bg-red-400 text-white text-sm md:text-md">
+                            Download Resume
+                        </a>
+                </div> 
             </div>
             <motion.div initial={{x:300,opacity:0}} animate={{x:0,opacity:1,scale:[.2,.4,.8],borderRadius: ["0", "15%", "25%"]}} transition={{duration:1,type:"spring"}} className="person order-1 md:w-1/2 md:mt-0 md:text-left text-center w-full overflow-hidden">
                 <img src={`${process.env.PUBLIC_URL}/me.jpg`} className="md:w-full" alt=""/>
